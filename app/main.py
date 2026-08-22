c72e8ffca7bc5114141fb01512e859bcb3cc769d
johnharke <57689898+johnharke@users.noreply.github.com>
2026-08-22 10:18:54 -0700

    health v1.0.1

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ class Order(BaseModel):
 @app.get("/health")
 def health():
     return {
-        "message": "Service is very healthy update v1.0.0",
+        "message": "Service is very healthy update v1.0.1",
         "status": "healthy",
         "version": VERSION,
         "environment": "dev"
